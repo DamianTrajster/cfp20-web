b1bfb37573f6440c87a903c0545c1e68e4b732c7
DamianTrajster <74788379+DamianTrajster@users.noreply.github.com>
2022-09-22 19:52:29 -0300

    add slider

---
+++ js/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    //Owl
+  
+    $('#project-slider').owlCarousel({
+        loop: true,
+        nav: false,
+        items: 2,
+        dots: true,
+        smartSpeed: 600,
+        center: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2,
+                margin: 8,
+            }
+        }
+    })
+
+    $('.reviews-slider').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        smartSpeed: 900,
+        items: 1,
+        margin: 24,
+        autoplay: true,
+        autoplayTimeout: 4000,
+    })
+});
